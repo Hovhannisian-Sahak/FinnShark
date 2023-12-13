ddc999629ae3a61b2e292c18de8a1c14dc530791
Sahak <eversuperskinny@gmail.com>
2023-12-13 23:40:41 +0400

    adding tailwind, and creating routes

--- frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -1,5 +1,6 @@
 import { SyntheticEvent } from "react";
 import CardPortfolio from "../CardPortfolio/CardPortfolio";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   portfolioValues: string[];
@@ -8,9 +9,11 @@ interface Props {
 
 const ListPortfolio = ({ portfolioValues, onPortfolioDelete }: Props) => {
   return (
-    <>
-      <h2>My Portfolio</h2>
-      <div>
+    <section id="portfolio">
+      <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
+        My Portfolio
+      </h2>
+      <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
         <>
           {portfolioValues.length > 0 ? (
             portfolioValues.map((portfolioValue) => {
@@ -22,11 +25,13 @@ const ListPortfolio = ({ portfolioValues, onPortfolioDelete }: Props) => {
               );
             })
           ) : (
-            <h3>Your portfolio is empty.</h3>
+            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+              Your portfolio is empty.
+            </h3>
           )}
         </>
       </div>
-    </>
+    </section>
   );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -10,6 +10,6 @@ export default defineConfig(({ mode }) => {
         env.VITE_REACT_APP_API_KEY
       ),
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
   };
 });
